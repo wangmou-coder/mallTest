146029384ae7c0c4cb7b6bce2909e78162e72f47
wang <wangshibo9809@163.com>
2020-10-08 11:43:24 +0800

    master

---
+++ supermall/src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" :key="index" 
+    class="tab-control-item" 
+    :class="{active: index===cIndex}"
+    @click="itemclick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabControl',
+    props: {
+      titles: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        cIndex: 0
+      }
+    },
+    methods: {
+      itemclick(index){
+        this.cIndex = index
+      }
+    },
+  }
+
+</script>
+
+<style>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+    font-size: 14px;
+    background-color: #fff;
+  }
+
+  .tab-control-item {
+    flex: 1;
+  }
+  .tab-control-item span{
+    padding: 3px;
+  }
+  .active {
+    color: var(--color-high-text);
+  }
+  .active span{
+    
+    border-bottom: 2px solid var(--color-high-text);
+  }
+
+</style>
---
+++ supermall/src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendView',
+    props: {
+      recommends: {
+        type: Array,
+        default () {
+          return []
+        }
+
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .recommend {
+    display: flex;
+    text-align: center;
+    font-size: 13px;
+    padding: 10px 0 20px;
+    border-bottom: 8px solid #eeeeee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 75px;
+    height: 75px;
+    margin-bottom: 2px;
+  }
+
+</style>
